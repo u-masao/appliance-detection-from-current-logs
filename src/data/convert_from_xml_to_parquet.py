7d36aae31a6b57694c66552fb0ad2c7928687a2c
u-masao <4973920+u-masao@users.noreply.github.com>
2024-12-24 18:29:46 +0900

    refactor: Improve logging and parameter handling in XML to Parquet conversion

--- src/data/convert_from_xml_to_parquet.py
+++ src/data/convert_from_xml_to_parquet.py
@@ -1,11 +1,9 @@
-import click
 import logging
+
+import click
 import mlflow
 import pandas as pd
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 def convert_xml_to_parquet(xml_path: str, parquet_path: str):
     """
@@ -14,6 +12,7 @@ def convert_xml_to_parquet(xml_path: str, parquet_path: str):
     :param xml_path: Path to the input XML file.
     :param parquet_path: Path to the output Parquet file.
     """
+    logger = logging.getLogger(__name__)
     # Read the XML file into a DataFrame
     df = pd.read_xml(xml_path)
 
@@ -23,17 +22,21 @@ def convert_xml_to_parquet(xml_path: str, parquet_path: str):
 
     # Log the number of rows and columns
     num_rows, num_cols = df.shape
-    logger.info("Number of rows: %d, Number of columns: %d", num_rows, num_cols)
+    logger.info(
+        "Number of rows: %d, Number of columns: %d", num_rows, num_cols
+    )
     mlflow.log_param("num_rows", num_rows)
     mlflow.log_param("num_cols", num_cols)
 
     # Write the DataFrame to a Parquet file
     df.to_parquet(parquet_path)
 
+
 @click.command()
-@click.argument('xml_file_path')
-@click.argument('parquet_file_path')
-def main(xml_file_path, parquet_file_path):
+@click.argument("xml_file_path", type=click.Path(exists=True))
+@click.argument("parquet_file_path", type=click.Path())
+@click.option("--mlflow_run_name", type=str, default="develop")
+def main(**kwargs):
     """
     Convert an XML file to a Parquet file.
 
@@ -41,7 +44,23 @@ def main(xml_file_path, parquet_file_path):
     :param parquet_file_path: Path to the output Parquet file.
     """
 
-    convert_xml_to_parquet(xml_file_path, parquet_file_path)
+    logger = logging.getLogger(__name__)
+    logger.info("==== start process ====")
+    logger.info({f"args.{k}": v for k, v in kwargs.items()})
+
+    mlflow.set_experiment("make_dataset")
+    mlflow.start_run(run_name=kwargs["mlflow_run_name"])
+    mlflow.log_params({f"args.{k}": v for k, v in kwargs.items()})
+
+    convert_xml_to_parquet(
+        kwargs["xml_file_path"], kwargs["parquet_file_path"]
+    )
+
+    mlflow.end_run()
+    logger.info("==== end process ====")
+
 
 if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
     main()

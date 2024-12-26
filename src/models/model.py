95a9d304bbfa011b2230195cd047200334329127
u-masao <4973920+u-masao@users.noreply.github.com>
2024-12-26 20:27:16 +0900

    code formatting

--- src/models/model.py
+++ src/models/model.py
@@ -8,7 +8,9 @@ class TransformerModel(nn.Module):
     ):
         super(TransformerModel, self).__init__()
         self.embedding = nn.Linear(input_dim, embed_dim)
-        self.transformer = nn.Transformer(embed_dim, num_heads, num_layers, batch_first=True)
+        self.transformer = nn.Transformer(
+            embed_dim, num_heads, num_layers, batch_first=True
+        )
         self.fc_out = nn.Linear(embed_dim, output_dim)
 
     def forward(self, src):

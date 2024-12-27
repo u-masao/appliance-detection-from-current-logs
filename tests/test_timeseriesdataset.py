1269743e58ed3a625e180713fc71c77b523bc809
u-masao <4973920+u-masao@users.noreply.github.com>
2024-12-27 21:03:31 +0900

    dataset test

--- tests/test_timeseriesdataset.py
+++ tests/test_timeseriesdataset.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import torch
+
 from src.models.dataset import TimeSeriesDataset
+
 
 def test_timeseriesdataset():
     # サンプルデータを作成
     data = {
         "feature1": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
         "feature2": [2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-        "gap": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        "gap": [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
         "watt_black": [10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
         "watt_red": [15, 25, 35, 45, 55, 65, 75, 85, 95, 105],
         "watt_kitchen": [20, 30, 40, 50, 60, 70, 80, 90, 100, 110],
@@ -21,10 +23,12 @@ def test_timeseriesdataset():
     target_columns = ["watt_black", "watt_red", "watt_kitchen", "watt_living"]
 
     # データセットを作成
-    dataset = TimeSeriesDataset(df, input_length, output_length, target_columns)
+    dataset = TimeSeriesDataset(
+        df, input_length, output_length, target_columns
+    )
 
     # データセットの長さをテスト
-    assert len(dataset) == 6  # 有効なインデックスは0から5まで
+    assert len(dataset) == 3  # 有効なインデックスは0から5まで
 
     # データセットのアイテムをテスト
     x, y = dataset[0]
@@ -39,4 +43,9 @@ def test_timeseriesdataset():
             dtype=torch.float32,
         ),
     )
-    assert torch.equal(y, torch.tensor([[40, 45, 50, 55], [50, 55, 60, 65]], dtype=torch.float32))
+    assert torch.equal(
+        y,
+        torch.tensor(
+            [[40, 45, 50, 55], [50, 55, 60, 65]], dtype=torch.float32
+        ),
+    )

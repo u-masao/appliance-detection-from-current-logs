8429a803a0e77894944bb3f8b5bcd925a5168b70
u-masao <4973920+u-masao@users.noreply.github.com>
2024-12-27 21:05:13 +0900

    code formatting

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -12,18 +12,16 @@ class TimeSeriesDataset(Dataset):
         self.data = data
         self.input_length = input_length
         self.output_length = output_length
-
-    def __init__(self, data, input_length, output_length, target_columns):
-        self.target_columns = target_columns
-        self.data = data
-        self.input_length = input_length
-        self.output_length = output_length
         self.valid_indices = self._compute_valid_indices()
 
     def _compute_valid_indices(self):
         valid_indices = []
-        for idx in range(len(self.data) - self.input_length - self.output_length + 1):
-            xy_df = self.data.iloc[idx : idx + self.input_length + self.output_length]
+        for idx in range(
+            len(self.data) - self.input_length - self.output_length + 1
+        ):
+            xy_df = self.data.iloc[
+                idx : idx + self.input_length + self.output_length
+            ]
             if xy_df["gap"].sum() == 0:
                 valid_indices.append(idx)
         return valid_indices
@@ -36,9 +34,14 @@ class TimeSeriesDataset(Dataset):
         x_df = self.data.iloc[idx : idx + self.input_length]
         x = x_df.drop("gap", axis=1).to_numpy(dtype="float32")
         y = self.data.iloc[
-            idx + self.input_length : idx + self.input_length + self.output_length
+            idx
+            + self.input_length : idx
+            + self.input_length
+            + self.output_length
         ][self.target_columns].to_numpy(dtype="float32")
-        return torch.tensor(x, dtype=torch.float32), torch.tensor(y, dtype=torch.float32)
+        return torch.tensor(x, dtype=torch.float32), torch.tensor(
+            y, dtype=torch.float32
+        )
 
 
 def load_data(file_path, fraction=1.0):

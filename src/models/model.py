86f7e1578c853e496949bfa5c5206c3fe057d1ff
u-masao <4973920+u-masao@users.noreply.github.com>
2024-12-30 10:09:35 +0900

    torch.load() change

--- src/models/model.py
+++ src/models/model.py
@@ -174,7 +174,7 @@ def save_model(model, path, model_config=None):
 
 def load_model(path):
     """Load a TimeSeriesModel and its configuration from the specified path."""
-    checkpoint = torch.load(path, weights_only=True)
+    checkpoint = torch.load(path, weights_only=False)
     model_config = checkpoint["config"]
     model = create_model(
         input_sequence_length=model_config["input_sequence_length"],

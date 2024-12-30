acedfcf6a5cc76631fb2325df718e47e847c2741
u-masao <4973920+u-masao@users.noreply.github.com>
2024-12-30 11:35:09 +0900

    bugfix, model_config

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -7,20 +7,31 @@ from torch.utils.data import Dataset
 
 # Custom Dataset
 class TimeSeriesDataset(Dataset):
-    def __init__(self, data, input_length, output_length, target_columns):
+    def __init__(
+        self,
+        data,
+        input_sequence_length,
+        output_sequence_length,
+        target_columns,
+    ):
         self.target_columns = target_columns
         self.data = data
-        self.input_length = input_length
-        self.output_length = output_length
+        self.input_sequence_length = input_sequence_length
+        self.output_sequence_length = output_sequence_length
         self.valid_indices = self._compute_valid_indices()
 
     def _compute_valid_indices(self):
         valid_indices = []
         for idx in range(
-            len(self.data) - self.input_length - self.output_length + 1
+            len(self.data)
+            - self.input_sequence_length
+            - self.output_sequence_length
+            + 1
         ):
             xy_df = self.data.iloc[
-                idx : idx + self.input_length + self.output_length
+                idx : idx
+                + self.input_sequence_length
+                + self.output_sequence_length
             ]
             if xy_df["gap"].sum() == 0:
                 valid_indices.append(idx)
@@ -31,13 +42,13 @@ class TimeSeriesDataset(Dataset):
 
     def __getitem__(self, index):
         idx = self.valid_indices[index]
-        x_df = self.data.iloc[idx : idx + self.input_length]
+        x_df = self.data.iloc[idx : idx + self.input_sequence_length]
         x = x_df.drop("gap", axis=1).to_numpy(dtype="float32")
         y = self.data.iloc[
             idx
-            + self.input_length : idx
-            + self.input_length
-            + self.output_length
+            + self.input_sequence_length : idx
+            + self.input_sequence_length
+            + self.output_sequence_length
         ][self.target_columns].to_numpy(dtype="float32")
         return torch.tensor(x, dtype=torch.float32), torch.tensor(
             y, dtype=torch.float32
